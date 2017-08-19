57bff3b7af4ce28c654db847f12e1ed11b219813
fj365 <abs4471@hotmail.com>
2017-08-19 17:12:19 +0800

    Update 5sing.php

--- php/5sing.php
+++ php/5sing.php
@@ -1,4 +1,5 @@
 <?php
+//http://data.5sing.kgimg.com/G056/M04/0A/15/GJQEAFbFsjSAUoE-AJgvL-q243g612.mp3
 preg_match('|"ticket":\s+"([^"]+)",|ims', file_get_contents("http://5sing.kugou.com/fc/14840042.html"), $c);
 $d=json_decode(base64_decode($c[1]),true);
 echo $d['file'];
