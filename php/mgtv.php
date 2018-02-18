022b6c2094af4fd691ba07e8c6b968f1f3e6d39c
fj365 <abs4471@hotmail.com>
2018-02-18 16:21:24 +0800

    Update mgtv.php

--- php/mgtv.php
+++ php/mgtv.php
@@ -6,12 +6,12 @@ function CURLS($url){
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+	//curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
 	curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
 	curl_setopt($ch, CURLOPT_REFERER, $url);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-FORWARDED-FOR:211.138.151.34', 'CLIENT-IP:211.138.156.66'));
-	curl_setopt($c, CURLOPT_COOKIE, 'sessionid=1514525461731; __STKUUID=f624f94b-f0ab-43a7-89fa-5a8d1d978929; MQGUID=946613877276545024; __MQGUID=946613877276545024; search-history=%u56E0%u4E3A%u7231%2C%u6728%u4E43%u4F0A; lastActionTime=1514526310223');
+	//curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-FORWARDED-FOR:211.138.151.34', 'CLIENT-IP:211.138.156.66'));
+	//curl_setopt($c, CURLOPT_COOKIE, 'sessionid=1514525461731; __STKUUID=f624f94b-f0ab-43a7-89fa-5a8d1d978929; MQGUID=946613877276545024; __MQGUID=946613877276545024; search-history=%u56E0%u4E3A%u7231%2C%u6728%u4E43%u4F0A; lastActionTime=1514526310223');
 	$data = curl_exec($ch);
 	curl_close($ch);
 	return $data;
@@ -35,7 +35,10 @@ if(strstr($uu,'/dianying/')){
 	preg_match('|http://\w+.titan.mgtv.com/(.*)_(\d+)/(.*)_201(.*)_mp4/|ims',$uu,$c);
 	$mp4 = 'http://disp.titan.mgtv.com/vod.do?fmt=4&pno=2010&fid='.$c[3].'&file=/'.$c[1].'_'.$c[2].'/'.$c[3].'_201'.$c[4].'.mp4';
 }
+echo $uu.$mp4;
+/*
 header("Content-type: application/octet-stream");
 header("Content-Disposition:attachment;filename='QQ121027740_MGTV_".$_GET['vid'].".mp4'");
 header("location:".$mp4);
+*/
 ?>
