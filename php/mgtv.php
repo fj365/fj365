fc3f34a52e7fc7e5f571a923f4d3e44cab1ab589
fj365 <abs4471@hotmail.com>
2018-02-18 16:36:47 +0800

    Update mgtv.php

--- php/mgtv.php
+++ php/mgtv.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(0);
+//error_reporting(0);
 header("Content-Type:text/html;charset=utf-8");
 $fname = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER["SCRIPT_NAME"];
 function CURLS($url){
